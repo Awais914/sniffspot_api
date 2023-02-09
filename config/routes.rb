c2c20a640dd6812e5f7355ccab44fc2ff18a059d
Awais914 <m.awais580@yahoo.com>
2023-02-10 01:12:56 +0500

    resolved conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-  get 'spots/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-=======
->>>>>>> Stashed changes
   namespace :api do
     namespace :v1 do
       resources :spots, except: [:destroy] do
         collection do
           get :sort
         end
-<<<<<<< Updated upstream
-      end
-    end
-  end
-=======
         resources :reviews, only:[:create, :update]
       end
     end
   end
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 end
