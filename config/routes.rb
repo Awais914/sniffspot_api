d0f26d1a46ac27d6918f9c19dca3e58784cb9b35
Awais914 <m.awais580@yahoo.com>
2023-02-09 17:54:14 +0500

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  # get 'spots/index'
   namespace :api do
     namespace :v1 do
-      resources :spots do
+      resources :spots, except: [:destroy] do
         collection do
           get :sort
         end
       end
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
